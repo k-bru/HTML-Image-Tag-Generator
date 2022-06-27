497435e6a1837d6aa89dc9b2cb531838d64eda22
Keegan Brunmeier <100226050+k-bru@users.noreply.github.com>
2022-06-27 19:17:19 -0400

    Add files via upload

---
+++ html-image-tag-generator.py
@@ -0,0 +1,18 @@
+from PIL import Image
+from tkinter.filedialog import askopenfilename
+import pyperclip
+
+#File user prompt
+filename = askopenfilename()
+
+#Open file
+im = Image.open(filename)
+
+#Assess width and height
+w, h = im.size
+
+#Generate string
+imageTag = f"<img src=\"{filename}\" width=\"{w}\" height=\"{h}\" alt=\"\">"
+
+#Copy to clipboard
+pyperclip.copy(imageTag)
