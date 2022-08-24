d5e6bfcaf7032a178463f602f10e3bcc7a13b121
Keegan Brunmeier <100226050+k-bru@users.noreply.github.com>
2022-08-23 23:14:29 -0400

    Added comments and changed path options
    Changed default path from absolute to relative, added comments for those who wish to change the path.

--- html-image-tag-generator.py
+++ html-image-tag-generator.py
@@ -2,17 +2,30 @@ from PIL import Image
 from tkinter.filedialog import askopenfilename
 import pyperclip
 
+#This program assumes a simple folder structure in which the images are stored within a single subfolder stored in the same location as index.html
+#Can be simply edited to your own path after pasting results, code is simple enough to fully edit it to your own path
+#Example 
+# "images/imagename.png" -GOOD
+# "images/smallimages/imagename.png" -Will only copy "smallimages/imagename.png" to clipboard
+
 #File user prompt
 filename = askopenfilename()
 
 #Open file
 im = Image.open(filename)
 
+#Split the path, only using the final two indexes (folder/filename.png)
+filename = filename.split("/")
+
+#If you need to go add an additional folder, simple add "{filename[-3]}/" at the start of the f-string. Repeat with additional indexes if necessary
+string = f"{filename[-2]}/{filename[-1]}"
+
 #Assess width and height
 w, h = im.size
 
 #Generate string
-imageTag = f"<img src=\"{filename}\" width=\"{w}\" height=\"{h}\" alt=\"\">"
+#Alt text will be blank
+imageTag = f"<img src=\"{string}\" width=\"{w}\" height=\"{h}\" alt=\"\">"
 
 #Copy to clipboard
 pyperclip.copy(imageTag)
